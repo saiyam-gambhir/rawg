a470f2d9344be04533a085a4889e887b6a72bbb5
Saiyam Gambhir <saiyam@syncio.co>
2024-07-29 02:18:16 +0530

    Game card skeleton

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,16 +1,16 @@
-import { Card, CardBody, Heading, HStack, Image, Text } from "@chakra-ui/react"
+import { Card, CardBody, Heading, HStack, Image } from "@chakra-ui/react"
 import { Game } from "../hooks/useGames"
 import PlatformIconList from "./PlatformIconList"
 import CriticScore from "./CriticScore"
 import getCroppedImageurl from "../services/image-url"
 
-interface GameProps {
+interface Props {
   game: Game
 }
 
-const GameCard = ({ game }: GameProps) => {
+const GameCard = ({ game }: Props) => {
   return (
-    <Card borderRadius={10} overflow="hidden">
+    <Card>
       <Image src={getCroppedImageurl(game.background_image)}></Image>
       <CardBody>
         <Heading fontSize="lg">{game.name}</Heading>
