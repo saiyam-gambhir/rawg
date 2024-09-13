1f37b97bebfa7e9c9fbfd576ced003199f5f69fa
Saiyam Gambhir <saiyam@syncio.co>
2024-09-13 21:11:11 +0530

    Show a spinner for genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,7 +1,14 @@
+import { Spinner } from "@chakra-ui/react";
 import useGenres from "../hooks/useGenres"
 
 const GenreList = () => {
-  const { data } = useGenres();
+  const { data, isLoading, error } = useGenres();
+
+  if(error) return null;
+
+  if(isLoading) {
+    return <Spinner />
+  }
 
   return (
     <div>
