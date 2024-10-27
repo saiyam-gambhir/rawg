34c44c09f0e839be8dab4b356ef755e88303acd9
gambhir-saiyam <saiyam1290@gmail.com>
2024-10-27 20:52:01 +0530

    Build game attributes

---
+++ src/components/GameAttributes.tsx
@@ -0,0 +1,41 @@
+import { SimpleGrid, Text } from '@chakra-ui/react';
+import CriticScore from './CriticScore';
+import DefinationItem from './DefinationItem';
+import { Game } from '../entities/Game';
+
+interface Props {
+  game: Game;
+}
+
+const GameAttributes = ({ game }: Props) => {
+  return (
+    <SimpleGrid
+      columns={2}
+      as="dl"
+    >
+      <DefinationItem term="Platform">
+        {game.parent_platforms.map(({ platform }) => (
+          <Text key={platform.id}>{platform.name}</Text>
+        ))}
+      </DefinationItem>
+
+      <DefinationItem term="Metascore">
+        <CriticScore score={game.metacritic} />
+      </DefinationItem>
+
+      <DefinationItem term="Genre">
+        {game.genres.map((genre) => (
+          <Text key={genre.id}>{genre.name}</Text>
+        ))}
+      </DefinationItem>
+
+      <DefinationItem term="Publishers">
+        {game.publishers.map((publisher) => (
+          <Text key={publisher.id}>{publisher.name}</Text>
+        ))}
+      </DefinationItem>
+    </SimpleGrid>
+  );
+};
+
+export default GameAttributes;
--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -2,6 +2,7 @@ import { Box, Heading, Spinner } from '@chakra-ui/react';
 import { useParams } from 'react-router-dom';
 import useGame from '../hooks/useGame';
 import ExpandableText from '../components/ExpandableText';
+import GameAttributes from '../components/GameAttributes';
 
 const GameDetailPage = () => {
   const { slug } = useParams();
@@ -15,6 +16,7 @@ const GameDetailPage = () => {
     <Box padding={5}>
       <Heading>{game.name}</Heading>
       <ExpandableText>{game.description_raw}</ExpandableText>
+      <GameAttributes game={game} />
     </Box>
   );
 };
